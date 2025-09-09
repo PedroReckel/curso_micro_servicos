233a5894d9d564c9c06b4712dbd9440d5a9927e3
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2025-09-09 19:59:00 -0300

    Utilizando Authentication Provider e filter chain

--- Extras/sbootxp-sercurity/src/main/java/io/github/cursomicroservicos/sbootxp_sercurity/config/SecurityConfig.java
+++ Extras/sbootxp-sercurity/src/main/java/io/github/cursomicroservicos/sbootxp_sercurity/config/SecurityConfig.java
@@ -5,15 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http, SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider, CustomFilter customFilter) throws Exception {
         return http
                 .authorizeHttpRequests(customizer -> {
                     customizer.requestMatchers("/public").permitAll(); // Quando a URL tiver o public significa que eu vou permitir o acesso
@@ -22,7 +29,31 @@ public class SecurityConfig {
                 })
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
+                .authenticationProvider(senhaMasterAuthenticationProvider)
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails commonUser = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(commonUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
